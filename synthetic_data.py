c085f27c76f3b30cdb265a00de379224869839f7
surfaceowl <chris@surfaceowl.com>
2023-10-05 14:57:40 -0700

    update slides + graphic; cleanup synthetic data script + gzipped data

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,71 +1,71 @@
-"""
-simple script to generate a healty amount of synthetic data for testing pandas and polars with
-note - the output is .gitignored to keep repo slim
-"""
 import numpy as np
 import pandas as pd
 from faker import Faker
 from pathlib import Path
+import gzip
+import shutil
 
-# Set seed for reproducibility
-seed = 42
-np.random.seed(seed)
-Faker.seed(seed)
+def generate_synthetic_data(num_records: int, seed: int = 77) -> pd.DataFrame:
+    """
+    Generate synthetic data for testing.
+    """
+    np.random.seed(seed)
+    Faker.seed(seed)
+    fake = Faker()
+    
+    # Generate data
+    customer_ids = [fake.passport_number() for _ in range(num_records)]
+    ages = np.random.normal(40, 15, num_records).astype(int)
+    lucky_numbers = np.random.poisson(lam=1.0, size=num_records).astype(int)
+    
+    occupations = np.random.choice([...], num_records)  # List of occupations
+    membership_statuses = np.random.choice([...], num_records)  # List of statuses
+    educations = np.random.choice([...], num_records)  # List of educations
+    date_started_python = pd.to_datetime([...])  # List of dates
+    
+    # Create DataFrame
+    df = pd.DataFrame({
+        "customer_id": customer_ids,
+        "age": ages,
+        "their_lucky_number": lucky_numbers,
+        "occupation": occupations,
+        "psf_membership_status": membership_statuses,
+        "education": educations,
+        "date_started_python": date_started_python
+    })
+    
+    return df
 
-# Set up DataFrame
-num_records = 50_000_000
+def gzip_csv(file_path: str) -> None:
+    """
+    Compresses a CSV file using gzip and saves it in the same directory.
+    """
+    source_path = Path(file_path)
+    compressed_file_path = source_path.with_suffix('.csv.gz')
+    
+    if not compressed_file_path.is_file():
+        with source_path.open('rb') as f_in, gzip.open(compressed_file_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        print(f"Compressed file saved as {compressed_file_path}")
+    else:
+        print("Zipped copy of datafile already exists")
 
-# Generate customer IDs efficiently using a NumPy array
-fake = Faker()
-customer_ids = [fake.passport_number() for _ in range(num_records)]
-df = pd.DataFrame({"customer_id": customer_ids})
+def main():
+    data_dir = Path("data")
+    csv_filename = data_dir / "python_dev_universe.csv"
+    gzipped_csv_filename = data_dir / "python_dev_universe.csv.gz"
+    parquet_filename = data_dir / "python_dev_universe.parquet"
+    
+    data_dir.mkdir(parents=True, exist_ok=True)
+    
+    if not csv_filename.is_file() and not gzipped_csv_filename.is_file() and not parquet_filename.is_file():
+        df = generate_synthetic_data(num_records=50_000_000)
+        df.to_csv(csv_filename, index=False)
+        df.to_parquet(parquet_filename, index=False)
+        gzip_csv(csv_filename)
+    else:
+        print("Test data exists in multiple formats.")
+        # Add file size info here
 
-# Add numeric columns
-ages = np.random.normal(40, 15, num_records).astype(int)
-lucky_numbers = np.random.poisson(lam=1.0, size=num_records).astype(int)
-df["age"] = ages
-df["their_lucky_number"] = lucky_numbers
-
-# Add categorical columns
-occupations = [
-    "Python Dev",
-    "Data Engineer",
-    "Data Scientist",
-    "Machine Learning Eng",
-    "DevOps Savior",
-    "Pandas Guru",
-    "Polars Guru",
-    "Apache Arrow Understudy",
-    "Rustacean",
-]
-membership_statuses = ["Supporting", "Managing", "Contributing", "Fellow", "Not Yet a Member"]
-educations = ["High School", "College", "Graduate", "Leetcode + sweat"]
-
-df["occupation"] = np.random.choice(occupations, num_records)
-df["psf_membership_status"] = np.random.choice(membership_statuses, num_records)
-df["education"] = np.random.choice(educations, num_records)
-
-# Add datetime column
-date_started_python = [
-    fake.date_of_birth(minimum_age=12, maximum_age=100) for _ in range(num_records)
-]
-df["date_started_python"] = pd.to_datetime(date_started_python)
-
-# Define file paths using pathlib
-data_dir = Path("data")
-csv_filename = data_dir / "python_dev_universe.csv"
-parquet_filename = data_dir / "python_dev_universe.parquet"
-
-# Create the data directory if it doesn't exist
-data_dir.mkdir(parents=True, exist_ok=True)
-
-# Save to disk using pathlib
-df.to_csv(csv_filename, index=False)
-df.to_parquet(parquet_filename, index=False)
-
-# Print size of files
-def get_file_size(filename):
-    return round(filename.stat().st_size / (1024 ** 3), 2)
-
-print(f"Size of csv: {get_file_size(csv_filename)} GB")
-print(f"Size of parquet: {get_file_size(parquet_filename)} GB")
+if __name__ == "__main__":
+    main()
